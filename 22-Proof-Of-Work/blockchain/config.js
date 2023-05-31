ef6385547fe1e790dbce8033f3c60140263a00c1
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2023-05-31 11:59:38 +0200

    Added solution for Proof Of Work

---
+++ 22-Proof-Of-Work/blockchain/config.js
@@ -0,0 +1,13 @@
+const MINE_RATE = 1000;
+const INITIAL_DIFFICULTY = 3;
+
+const GENESIS_DATA = {
+  timestamp: 1,
+  data: [],
+  hash: '#1',
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  lastHash: '######',
+};
+
+module.exports = { GENESIS_DATA, MINE_RATE };
