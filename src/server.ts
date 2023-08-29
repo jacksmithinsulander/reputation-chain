2303fa14622e1ce262735f67f1f5bb4bfa484556
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-08-29 21:24:56 +0200

    fixed some more on the server

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,27 @@ app.post('/api/transaction', (req: Request, res: Response) => {
     res.status(201).json({success: true, data: `Block Index: ${index}`});
 });
 
-app.get('/api/mine', (req: Request, res: Response) => {});
+app.get('/api/mine', (req: Request, res: Response) => {
+    const previousBlock: Block = reputationChain.getLastBlock();
+    const previousHash: string = previousBlock.hash;
+    type data = {
+        data: Transaction,
+        index: number
+    }
+    const data = {
+        data: reputationChain.pendingList,
+        index: previousBlock.index + 1
+    }
+    const nonce: number = reputationChain.proofOfWork(previousHash, data);
+    const hash: string = reputationChain.createHash(previousHash, data, nonce);
+    const block: Block = reputationChain.createBlock(nonce, previousHash, hash);
+
+    res.status(200).json({
+        success: true,
+        data: block
+    });
+
+});
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
 
