a6f57ae77ea1d9d6278a3f2ba4a4a6a11e4c0eac
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-08-29 13:13:25 +0200

    more tests

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,6 @@ import Blockchain from './blockchain.js';
 const myChain = new Blockchain();
 
 myChain.createBlock(1, 'PrevHash', 'CurrentHash')
-
+myChain.createBlock(2, 'PrevHash', 'CurrentHash')
+myChain.createBlock(3, 'PrevHash', 'CurrentHash')
 console.log(myChain)
