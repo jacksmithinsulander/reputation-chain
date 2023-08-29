a1aad2d12c7f25d8fb9cf84e956d869f216f34d3
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-08-29 13:37:59 +0200

    Added some more tests

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,23 @@ import Blockchain from './blockchain.js';
 
 const myChain = new Blockchain();
 
-myChain.createBlock(1, 'PrevHash', 'CurrentHash')
+myChain.createBlock(1, 'PrevHash', 'CurrentHash');
 //myChain.createBlock(2, 'PrevHash', 'CurrentHash')
 //myChain.createBlock(3, 'PrevHash', 'CurrentHash')
 
 myChain.addTransaction(100, 'Jack', 'Michael');
 
+myChain.createBlock(2, 'PrevHash', 'CurrentHash');
+
+myChain.addTransaction(100, 'Jack', 'Michael');
+myChain.addTransaction(143, 'Stefan', 'Björn');
+myChain.addTransaction(92, 'Björn', 'Michael');
+myChain.addTransaction(14, 'Jack', 'Björn');
+myChain.addTransaction(111, 'Michael', 'Stefan');
+
+myChain.createBlock(3, 'PrevHash', 'CurrentHash');
 
 console.log(myChain);
+console.log(myChain.chain.at(1));
+console.log(myChain.chain.at(2));
 //console.log(myChain.getLastBlock());
