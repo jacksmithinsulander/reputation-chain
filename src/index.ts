6b3e36eddcc4743b9183b802394c37c77d4abfcb
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-08-29 12:41:30 +0200

    redid the blockchain with strong typing

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+type Block = {
+    index: number,
+    timestamp: number,
+    data: string,
+    nonce: number,
+    hash: string,
+    previousHash: string
+};
+
+
+function Blockchain() {
+    this.chain = [];
+    this.pendingList = [];
+};
+
+Blockchain.prototype.createBlock = function(
+    nonce: number,
+    previousHash: string, 
+    hash: string
+): Block {
+    const newBlock: Block = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        data: this.pendingList,
+        nonce,
+        hash,
+        previousHash
+    }
+    this.pendingList = [];
+    this.chain.push(newBlock);
+    return newBlock;
+};
