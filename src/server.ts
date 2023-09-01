6fb062ef87ff2dd2176ed28e8233990b29c2bd62
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-09-01 15:11:08 +0200

    server file up to date

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import blockchain from './routes/blockchain-routes.js';
 import block from './routes/block-routes.js';
+import consensus from './routes/consensus-routes.js';
+import node from './routes/node-routes.js'
 import transaction from './routes/transaction-routes.js';
+import transactions from './routes/transactions-routes.js';
 
 const app = express();
 
@@ -12,8 +15,8 @@ app.use(express.json());
 app.use('/api/v1/blockchain', blockchain);
 app.use('/api/v1/block', block);
 app.use('/api/v1/transaction', transaction);
-//app.use('/api/v1/transactions', transactions);
-//app.use('/api/v1/node', node);
-//app.use('/api/v1/consensus', consensus);
+app.use('/api/v1/transactions', transactions);
+app.use('/api/v1/node', node);
+app.use('/api/v1/consensus', consensus);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
